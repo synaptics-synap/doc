ad0ed481423360a23f51849a6dd6ee367ea1caae
Lorenzo Keller <lorenzo.keller@synaptics.com>
2025-05-22 14:35:19 +0200

    Switch to reusable workflows and theme

--- build.sh
+++ build.sh
@@ -4,13 +4,4 @@ set -e
 
 ROOT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-if [[ ! -d "${ROOT_DIR}/framework" ]] ; then
-   echo "WARNING: Framework git not found in ${ROOT_DIR}/framework, doxygen generation will not work"
-fi
-
-
-echo Building docker...
-
-IMAGE_ID=$(docker build -q ${ROOT_DIR}/.github/actions/build-doc)
-
-docker run --rm -e GITHUB_REF -v /:/host/ -w /host/${ROOT_DIR}/.. ${IMAGE_ID}
+docker run -e GITHUB_REF -e GITHUB_REPOSITORY --rm -v /:/host/ -w /host/${ROOT_DIR}/ ghcr.io/syna-astra-dev/synaptics-sphinx-theme/builder:latest
--- conf.py
+++ conf.py
@@ -1,63 +1,17 @@
-import os
-
-# setup documentation metadata
-
 project = 'SyNAP'
 copyright = '2024, Synaptics'
 author = 'Synaptics'
-release = os.environ.get('RELEASE_VER', 'git-main')
+release = '3.1.0'
 
-# setup html theme
-
-html_static_path = ['_static']
-
-exclude_patterns = ["README.rst"]
-
-extensions = [
-    'sphinx_rtd_theme',
-    'sphinxcontrib.plantuml',
-    'breathe',
-]
-
-html_theme = "sphinx_rtd_theme"
-
-templates_path = [ '_templates' ]
-
-html_context = {
-    'display_github': True,
-    'github_repo': 'synaptics-synap/doc',
-    'github_version': 'main',
-    'conf_py_path': '/',
-    'version': release,
-}
+extensions = [ 'synaptics_sphinx_theme' ]
 
 # setup inclusion of doxygen documentation extracted from sources
-
 breathe_projects = {
     'runtime': '_build/doxygen/xml'
 }
 
-breathe_default_project = 'runtime'
-
-# this code is used to substitute #xx# variables anywhere in the source, even
-# inside literal blocks and code blocks
-
-def preprocess_variables(app, docname, source):
-    for varname, value in app.config.preprocessor_variables.items():
-        source[0] = source[0].replace(varname, value)
-
-
-preprocessor_variables = {
-    "#release#": release
+html_theme_options = { 
+    'logo_only': False
 }
 
-
-def setup(app):
-
-    # add support for pre-processing
-    app.add_config_value('preprocessor_variables', {}, True)
-    app.connect('source-read', preprocess_variables)
-
-    # add custom theme overrides and javascript
-    app.add_css_file('css/synaptics.css')
-    app.add_js_file('js/synaptics.js')
+breathe_default_project = 'runtime'
